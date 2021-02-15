455683d24c26536d6423ba013fda6094aea32b25
jsjackson <jsjackson@example.com>
2021-02-15 15:37:02 +1100

    Add custom user profile model, manager and migrations

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# configure django to use the overriden user model (instead of the one provided by django)
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
